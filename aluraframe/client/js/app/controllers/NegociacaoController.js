52ac3231b6b2d30f17eb961cf3246576dc100387
Frafaelrls <frafaelrls@gmail.com>
2023-03-01 17:55:48 -0300

    coletando o input do formulario

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,6 +2,16 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        alert('Foi clicado!');
+        
+        // O método .bind criar uma função que o seu this tem referência ao atributo fornecido
+        // Nes caso, a variável $ mantem a associação ao document
+        let $ = document.querySelector.bind(document);
+        let inputData = $('#data');
+        let inputQunatidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQunatidade.value);
+        console.log(inputValor.value);
     };
 };
