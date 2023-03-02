6be6cd556216fcff5dcb18277fd4461591151d10
Frafaelrls <frafaelrls@gmail.com>
2023-03-02 15:11:53 -0300

    adicionando função map para decrementar o segundo item do array que contem a data

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -20,7 +20,18 @@ class NegociacaoController {
         
         // Usando a expressão regular /-/g, onde, será verificado todas(g) as ocorrências 
         // e serão subsistidas por ','
-        let data = new Date(this.#inputData.value.replace(/-/g, ','));
+        //let data = new Date(this.#inputData.value.replace(/-/g, ','));
+
+        // O três pontos abaixo é um spread operator, onde cada ponto irá receber um item 
+        // de um array (semelhante ao desempacotamento de tuplas do Python)
+        // A função map() irá percorrer todos os itens do array chanmando a função em cada item
+        let data = new Date(...
+            this.#inputData.value
+            .split('-')
+            .map(function(item, indice){
+                return item - indice % 2
+            })
+        );
         console.log(data);
     };
 };
