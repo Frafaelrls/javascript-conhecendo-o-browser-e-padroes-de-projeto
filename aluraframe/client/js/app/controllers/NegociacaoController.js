e484b1254259ae59d1921f0d388fa16a3dcbb8c9
Frafaelrls <frafaelrls@gmail.com>
2023-03-02 15:31:51 -0300

    criando o objeto negociacao

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -20,18 +20,28 @@ class NegociacaoController {
         
         // Usando a expressão regular /-/g, onde, será verificado todas(g) as ocorrências 
         // e serão subsistidas por ','
-        //let data = new Date(this.#inputData.value.replace(/-/g, ','));
+        // let data = new Date(this.#inputData.value.replace(/-/g, ','));
 
-        // O três pontos abaixo é um spread operator, onde cada ponto irá receber um item 
+        // Os três pontos abaixo é um spread operator, onde cada ponto irá receber um item 
         // de um array (semelhante ao desempacotamento de tuplas do Python)
-        // A função map() irá percorrer todos os itens do array chanmando a função em cada item
-        let data = new Date(...
-            this.#inputData.value
+
+        // A função map() irá percorrer todos os itens do array chamando a função em cada item
+
+        // Temos também o uso da arrow functions(função de flecha) representada por =>
+        // Quando temos apenas uma unica instrução em uma arrow funcion podemos omitir o bloco
+        // Por termos apenas uma única instrução a arrow funcion já realiza o retorno da operação
+
+        let data = new Date(
+            ...this.#inputData.value
             .split('-')
-            .map(function(item, indice){
-                return item - indice % 2
-            })
+            .map((item, indice) => item - indice % 2)
         );
-        console.log(data);
+        let negociacao = new Negociacao(
+            data,
+            this.#inputQunatidade.value,
+            this.#inputValor.value
+        );
+
+        console.log(negociacao);
     };
 };
