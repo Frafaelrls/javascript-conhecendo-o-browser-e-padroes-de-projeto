92fb036e4ce48e6b2349ced49c90a1b3969c1f0c
Frafaelrls <frafaelrls@gmail.com>
2023-03-03 09:29:05 -0300

    refatorando método adiciona, fazendo o uso da classe DateHelper

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -18,34 +18,16 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        // Usando a expressão regular /-/g, onde, será verificado todas(g) as ocorrências 
-        // e serão subsistidas por ','
-        // let data = new Date(this.#inputData.value.replace(/-/g, ','));
+        let helper = new DateHelper()
 
-        // Os três pontos abaixo é um spread operator, onde cada ponto irá receber um item 
-        // de um array (semelhante ao desempacotamento de tuplas do Python)
-
-        // A função map() irá percorrer todos os itens do array chamando a função em cada item
-
-        // Temos também o uso da arrow functions(função de flecha) representada por =>
-        // Quando temos apenas uma unica instrução em uma arrow funcion podemos omitir o bloco
-        // Por termos apenas uma única instrução a arrow funcion já realiza o retorno da operação
-
-        let data = new Date(
-            ...this.#inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
         let negociacao = new Negociacao(
-            data,
+            helper.textoParaData(this.#inputData.value),
             this.#inputQunatidade.value,
             this.#inputValor.value
         );
 
-        let diaMesAno = negociacao.data.getDate()
-            + '/' + (negociacao.data.getMonth() + 1)
-            + '/' + negociacao.data.getFullYear();
+        console.log(negociacao);
 
-        console.log(diaMesAno);
+        console.log(helper.dataParaTexto(negociacao.data));
     };
 };
