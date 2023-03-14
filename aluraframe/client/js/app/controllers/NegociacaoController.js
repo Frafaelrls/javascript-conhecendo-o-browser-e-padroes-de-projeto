232250af33b45e7b16476ac66754521db177b599
Frafaelrls <frafaelrls@gmail.com>
2023-03-14 08:45:59 -0300

    renomeando atributo #inputQunatidade para #inputQuantidade

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,7 +2,7 @@ class NegociacaoController {
 
     // Atributos privados
     #inputData;
-    #inputQunatidade;
+    #inputQuantidade;
     #inputValor;
     #listaNegociacoes;
     #negociacoesView;
@@ -17,7 +17,7 @@ class NegociacaoController {
         let $ = document.querySelector.bind(document);
 
         this.#inputData = $('#data');
-        this.#inputQunatidade = $('#quantidade');
+        this.#inputQuantidade = $('#quantidade');
         this.#inputValor = $('#valor');
         
         this.#listaNegociacoes = new ListaNegociacoes();
@@ -49,14 +49,14 @@ class NegociacaoController {
 
         return new Negociacao(
             DateHelper.textoParaData(this.#inputData.value),
-            this.#inputQunatidade.value,
+            this.#inputQuantidade.value,
             this.#inputValor.value
         );
     }
 
     #limpaFormulario() {
         this.#inputData.value = '';
-        this.#inputQunatidade.value = 1;
+        this.#inputQuantidade.value = 1;
         this.#inputValor.value = 0.0;
 
         this.#inputData.focus();
